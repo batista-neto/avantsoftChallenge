c9860462bb6c51ab9a6414d1644305812abd7b3f
Joao Neto <jbssneto.develcode@rd.com.br>
2025-06-22 17:24:49 -0300

    done ReportService

---
+++ modules/analytics/business/impl/services/ReportServiceImpl.ts
@@ -0,0 +1,33 @@
+import { Report, ReportService } from "../..";
+import axios from '../../../../../axiosMock';
+
+export class ReportServiceImpl implements ReportService {
+    private baseUrl = process.env.EXPO_PUBLIC_BASE_URL;
+
+    async getSalesReport(): Promise<Report> {
+
+        try {
+            const response = await axios.get(`${this.baseUrl}/report`);
+            
+            const payload: Report = {
+                clients: [
+                    response.data.clients.map((client: any) => ({
+                        name: client.info.nomeCompleto,
+                        email: client.info.detalhes.email,
+                        birthDate: client.info.detalhes.nascimento,
+                        sales: client.estatisticas.vendas || []
+                    }))
+                ],
+                meta: {
+                    recordTotal: response.data.meta?.recordTotal || 0,
+                    page: response.data.meta?.page || 1
+                }
+            }
+
+            return payload;
+        } catch (error) {
+            throw new Error("Failed to fetch sales report");
+        }
+
+    }
+}
