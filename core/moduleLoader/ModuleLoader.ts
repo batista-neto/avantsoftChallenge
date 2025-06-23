db25e15e6172404bf1f6cbad4621567edb3a1620
Joao Neto <jbssneto.develcode@rd.com.br>
2025-06-22 23:26:25 -0300

    adding analytics business on provider

--- core/moduleLoader/ModuleLoader.ts
+++ core/moduleLoader/ModuleLoader.ts
@@ -1,6 +1,7 @@
 import { DIMapper } from "core/di/api";
 import AccountProvider from "modules/account/business/impl/main";
 import AccountProviderUi from "modules/account/ui/main";
+import AnalyticsProvider from "modules/analytics/business/impl/main";
 import AnalyticsProviderUi from "modules/analytics/ui/main";
 import { DIContainer } from "../di/api/DIContainer";
 export class ModuleLoader {
@@ -10,6 +11,7 @@ export class ModuleLoader {
         AccountProviderUi(container); 
 
         //Analytics module
+        AnalyticsProvider(container);
         AnalyticsProviderUi(container)
     }
 }
